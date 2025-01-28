928b7fe0034c7b728a19cf62c9101d89014c11c7
sNurseit <nurseit.taimas@gmail.com>
2025-01-28 12:08:09 +0500

    add constants for auth logic

--- src/main/java/com/nurseit/carproject/service/impl/AuthServiceImpl.java
+++ src/main/java/com/nurseit/carproject/service/impl/AuthServiceImpl.java
@@ -5,6 +5,7 @@ import com.nurseit.carproject.dto.LoginResponseDto;
 import com.nurseit.carproject.dto.RegistrationRequestDto;
 import com.nurseit.carproject.entity.Role;
 import com.nurseit.carproject.entity.User;
+import com.nurseit.carproject.exceptions.AuthException;
 import com.nurseit.carproject.repository.UserRepository;
 import com.nurseit.carproject.service.AuthService;
 import com.nurseit.carproject.service.RoleService;
@@ -16,6 +17,9 @@ import org.springframework.stereotype.Service;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
+
+import static com.nurseit.carproject.util.Constants.INVALID_USERNAME_OR_PASSWORD;
+import static com.nurseit.carproject.util.Constants.USERNAME_EXISTS;
 
 @Service
 @RequiredArgsConstructor
@@ -29,7 +33,7 @@ public class AuthServiceImpl implements AuthService {
     @Override
     public void registration(RegistrationRequestDto registrationRequest) {
         if (repository.findByUsername(registrationRequest.getUsername()).isPresent()) {
-            throw new IllegalArgumentException("User with the same username already exists");
+            throw new AuthException(USERNAME_EXISTS);
         }
         create(registrationRequest.getUsername(), registrationRequest.getPassword());
     }
@@ -38,14 +42,13 @@ public class AuthServiceImpl implements AuthService {
     private void create(String username, String password) {
         Set<Role> role = new HashSet<>();
         role.add(roleService.findByName("ROLE_USER"));
-        User user = User.builder()
-                .roles(role)
-                .isEnabled(true)
-                .username(username)
-                .password(passwordEncoder.encode(password))
-                .build();
         repository.save(
-                user
+                User.builder()
+                        .roles(role)
+                        .isEnabled(true)
+                        .username(username)
+                        .password(passwordEncoder.encode(password))
+                        .build()
         );
     }
 
@@ -53,11 +56,11 @@ public class AuthServiceImpl implements AuthService {
     public LoginResponseDto login(LoginRequestDto loginRequestDto) {
         Optional<User> userOptional = repository.findByUsername(loginRequestDto.getUsername());
         if (userOptional.isEmpty()) {
-            throw new IllegalArgumentException("Invalid username or password");
+            throw new AuthException(INVALID_USERNAME_OR_PASSWORD);
         }
         User user = userOptional.get();
         if (!passwordEncoder.matches(loginRequestDto.getPassword(), user.getPassword())) {
-            throw new IllegalArgumentException("Invalid username or password");
+            throw new AuthException(INVALID_USERNAME_OR_PASSWORD);
         }
 
         return new LoginResponseDto(jwtUtil.generateToken(user.getId(), user.getRoles()));
