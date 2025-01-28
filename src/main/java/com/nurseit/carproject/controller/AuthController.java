267148427b68b6ee267558630313adfc8e25dbfd
sNurseit <nurseit.taimas@gmail.com>
2025-01-28 12:07:13 +0500

    add validator

--- src/main/java/com/nurseit/carproject/controller/AuthController.java
+++ src/main/java/com/nurseit/carproject/controller/AuthController.java
@@ -1,9 +1,19 @@
 package com.nurseit.carproject.controller;
 
 
+import com.nurseit.carproject.dto.ErrorMessage;
 import com.nurseit.carproject.dto.LoginRequestDto;
+import com.nurseit.carproject.dto.LoginResponseDto;
 import com.nurseit.carproject.dto.RegistrationRequestDto;
+import com.nurseit.carproject.entity.Car;
 import com.nurseit.carproject.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
@@ -16,17 +26,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
+@Tag(name = "Auth Controller", description = "Operations related to auth")
 public class AuthController {
 
     private final AuthService service;
 
+
+    @Operation(summary = "Login")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully login",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = LoginResponseDto.class))),
+            @ApiResponse(responseCode = "500", description = "Username or password is incorrect",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+    })
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequestDto loginRequest) {
         return ResponseEntity.ok(service.login(loginRequest));
     }
 
     @PostMapping("/registration")
-    public ResponseEntity<?> registration(@RequestBody RegistrationRequestDto registrationRequest) {
+    public ResponseEntity<?> registration(@RequestBody @Valid RegistrationRequestDto registrationRequest) {
         service.registration(registrationRequest);
         return new ResponseEntity<>(HttpEntity.EMPTY, HttpStatus.OK);
     }
--- src/main/java/com/nurseit/carproject/dto/RegistrationRequestDto.java
+++ src/main/java/com/nurseit/carproject/dto/RegistrationRequestDto.java
@@ -1,11 +1,22 @@
 package com.nurseit.carproject.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class RegistrationRequestDto {
+    @NotBlank(message = "Username cannot be blank")
+    @Pattern(
+            regexp = "^[a-zA-Z0-9]+$",
+            message = "Username can only contain letters and numbers, without spaces"
+    )
     private String username;
+
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 5, message = "Password must be at least 5 characters long")
     private String password;
 }
